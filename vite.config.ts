617040503ddbb86d39758eefb30cfcf6e8c7dd18
Даниил Шило <crckhd10@gmail.com>
2022-10-11 23:03:52 +0300

    feat(linter): add stage-linter

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+/**
+ * Injecting SCSS import to string
+ * @path - Path to scss file
+ */
+function injectSCSSImport(path: string): string {
+  return '@import "' + path + '";';
+}
+
+const INDEX_SCSS = 'src/style.scss';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: { 
+      scss: {
+        additionalData: injectSCSSImport(INDEX_SCSS)
+      },
+    },
+  },
 });
