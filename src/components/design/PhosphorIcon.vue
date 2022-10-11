dff5854e4f48e37903f939582992dc340372556e
Даниил Шило <crckhd10@gmail.com>
2022-10-12 00:06:56 +0300

    feat(phosphor): remove phosphor-vue and add PhosphorIcon component

---
+++ src/components/design/PhosphorIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <i :class="`ph-${name}`" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+});
+</script>
+
+<style scoped lang="scss"></style>
